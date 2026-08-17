651fd12ccdd80b8a4b0097804a7208a71c6f4a28
laharii24 <sailaharikarakana4@gmail.com>
2026-08-17 19:17:05 +0530

    final outputs

--- src/test_api.py
+++ src/test_api.py
@@ -9,7 +9,7 @@ in a separate terminal:
 
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "http://127.0.0.1:8080"
 
 at_risk_customer = {
     "gender": "Female", "SeniorCitizen": 0, "Partner": "No", "Dependents": "No",
